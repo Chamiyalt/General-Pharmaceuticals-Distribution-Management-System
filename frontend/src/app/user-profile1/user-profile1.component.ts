b2525047c6934daccb403e93cb1f97675e8a6e17
Mihiran94 <mihiran.ponnamperuma@gmail.com>
2019-05-21 11:17:50 +0530

    add to frontend files

---
+++ frontend/src/app/user-profile1/user-profile1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile1',
+  templateUrl: './user-profile1.component.html',
+  styleUrls: ['./user-profile1.component.css']
+})
+export class UserProfile1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
