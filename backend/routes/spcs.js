477faea84c5ed3a41f758c8af67cbaba81600b6c
Mihiran94 <mihiran.ponnamperuma@gmail.com>
2019-05-25 01:47:05 +0530

    update function for the spcs.js

--- backend/routes/spcs.js
+++ backend/routes/spcs.js
@@ -21,3 +21,20 @@ router.post("", (req, res, next) => {
   });
 });
 
+
+//spc update
+router.put("/:id",(req,res,next) =>{
+  const spc = new Spc({
+    _id: req.body.id,
+    OutletName: req.body.OutletName,
+    OutletName: req.body.InChargeName,
+    Address: req.body.Address,
+    RegNum: req.body.RegNum,
+    Tel: req.body.Tel,
+    email: req.body.email
+  });
+  Spc.updateOne({_id: req.params.id},spc)
+  .then(result => {
+    res.status(200).json({message:'update successful!'});
+  });
+});
