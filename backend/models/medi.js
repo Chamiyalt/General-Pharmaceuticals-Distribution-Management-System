8e1ee9787e1a1bbe067ddf2e54dc56af00d8750f
Mihiran94 <mihiran.ponnamperuma@gmail.com>
2019-05-27 09:44:46 +0530

    create medi.js file backend

---
+++ backend/models/medi.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mediSchema = mongoose.Schema({
+  DrugName: { type: String, required: true },
+  category: { type: String, required: true },
+  code: { type: String, required: true },
+
+});
+
+module.exports = mongoose.model('Medi', spcSchema);
+
