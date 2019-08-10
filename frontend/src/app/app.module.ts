f64f0fbb49d975e01d26233ced3329129680c56a
Your Name <chaminlasantha@gmail.com>
2019-08-10 11:47:04 +0530

    m

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { ChartsModule } from 'ng2-charts/ng2-charts';
+
 
 
 import { AppRoutingModule } from './app.routing';
