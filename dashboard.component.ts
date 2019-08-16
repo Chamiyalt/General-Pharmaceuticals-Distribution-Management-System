efb59d94079fc4dde39775ee1c21f52649adae8d
vilashirangani <42765922+vilashirangani@users.noreply.github.com>
2019-08-16 07:45:07 +0530

    Add files via upload

---
+++ dashboard.component.ts
@@ -0,0 +1,302 @@
+import { Component, OnInit } from '@angular/core';
+import * as Chartist from 'chartist';
+declare var $: any;
+
+import { Drug } from "app/drug.model";
+import { Subscription } from 'rxjs';
+import { GetExpiresoonDrugsService } from 'app/get-expiresoon-drugs.service';
+import { GetHosStockService } from "../get-hos-stock.service";
+import { hosStock } from "app/getHosStock.model.1";
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  drugs: Drug[] = [];
+  private GetExpiresoonDrugsService: Subscription;
+  //chart here
+
+  private GetHosStockService: Subscription;
+
+  cityHos: hosStock[] = [];
+  selectedCityHos: hosStock[];
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+
+  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
+
+  //end chart
+
+  constructor(public DrugService: GetExpiresoonDrugsService ,public GetHosStockServic: GetHosStockService) { }
+  startAnimationForLineChart(chart){
+      let seq: any, delays: any, durations: any;
+      seq = 0;
+      delays = 80;
+      durations = 500;
+
+      chart.on('draw', function(data) {
+        if(data.type === 'line' || data.type === 'area') {
+          data.element.animate({
+            d: {
+              begin: 600,
+              dur: 700,
+              from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+              to: data.path.clone().stringify(),
+              easing: Chartist.Svg.Easing.easeOutQuint
+            }
+          });
+        } else if(data.type === 'point') {
+              seq++;
+              data.element.animate({
+                opacity: {
+                  begin: seq * delays,
+                  dur: durations,
+                  from: 0,
+                  to: 1,
+                  easing: 'ease'
+                }
+              });
+          }
+      });
+
+      seq = 0;
+  };
+  startAnimationForBarChart(chart){
+      let seq2: any, delays2: any, durations2: any;
+
+      seq2 = 0;
+      delays2 = 80;
+      durations2 = 500;
+      chart.on('draw', function(data) {
+        if(data.type === 'bar'){
+            seq2++;
+            data.element.animate({
+              opacity: {
+                begin: seq2 * delays2,
+                dur: durations2,
+                from: 0,
+                to: 1,
+                easing: 'ease'
+              }
+            });
+        }
+      });
+
+      seq2 = 0;
+  };
+  ngOnInit() {
+
+    this.DrugService.getDrugs();
+    this.GetExpiresoonDrugsService = this.DrugService.getDrugUpdateListener()
+      .subscribe((drugs: Drug[]) => {
+
+       
+        this.drugs = drugs;
+
+        
+      });
+      if(this.drugs){
+        this.showNotification('top','right');
+
+      }
+      
+
+
+
+      /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
+      
+      const dataDailySalesChart: any = {
+          labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+          series: [
+              [12, 17, 7, 17, 23, 18, 38]
+          ]
+      };
+
+     const optionsDailySalesChart: any = {
+          lineSmooth: Chartist.Interpolation.cardinal({
+              tension: 0
+          }),
+          low: 0,
+          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
+      }
+
+      var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+
+      this.startAnimationForLineChart(dailySalesChart);
+
+
+      /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
+
+      const dataCompletedTasksChart: any = {
+          labels: ['12p', '3p', '6p', '9p', '12p', '3a', '6a', '9a'],
+          series: [
+              [230, 750, 450, 300, 280, 240, 200, 190]
+          ]
+      };
+
+     const optionsCompletedTasksChart: any = {
+          lineSmooth: Chartist.Interpolation.cardinal({
+              tension: 0
+          }),
+          low: 0,
+          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
+      }
+
+      var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
+
+      // start animation for the Completed Tasks Chart - Line Chart
+      this.startAnimationForLineChart(completedTasksChart);
+
+
+
+      /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
+
+      var datawebsiteViewsChart = {
+        labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
+        series: [
+          [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
+
+        ]
+      };
+      var optionswebsiteViewsChart = {
+          axisX: {
+              showGrid: false
+          },
+          low: 0,
+          high: 1000,
+          chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
+      };
+      var responsiveOptions: any[] = [
+        ['screen and (max-width: 640px)', {
+          seriesBarDistance: 5,
+          axisX: {
+            labelInterpolationFnc: function (value) {
+              return value[0];
+            }
+          }
+        }]
+      ];
+      var websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
+
+      //start animation for the Emails Subscription Chart
+      this.startAnimationForBarChart(websiteViewsChart);
+
+
+
+
+
+
+
+
+
+this.GetHosStockServic.getHosStocks();
+this.GetHosStockService = this.GetHosStockServic.getStockUpdateListener()
+  .subscribe((hos: hosStock[]) => {
+
+    this.cityHos = [];
+    for (var x = 0; x < hos.length; x++) {
+      console.log(hos);
+     
+        console.log(hos);
+
+        
+
+     
+
+        if(Number(Number(hos[x].quentity ) / Number(hos[x].preQty) *100) < 30 ){
+          let modelData2 = {
+            id: hos[x].id,
+            Hname: hos[x].Hname,
+            DrugName: hos[x].DrugName,
+            quentity: hos[x].quentity,
+            preQty :hos[x].preQty,
+            per: Number(Number(hos[x].quentity ) / Number(hos[x].preQty) *100).toFixed(2),
+            city : hos[x].city
+           
+          };
+          this.showNotification2('top','right');
+          this.cityHos.push(modelData2);
+        }
+
+
+      
+    
+    }
+
+  });
+  }
+
+  showNotification(from, align){
+  
+    
+    $.notify({
+        icon: "danger",
+        message: "Check the expiration date on your medicines."
+
+    },{
+        type: 'danger',
+        timer: 4000,
+        placement: {
+            from: from,
+            align: align
+        },
+        template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+          '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+          '<i class="material-icons" data-notify="icon">notifications</i> ' +
+          '<span data-notify="title">{1}</span> ' +
+          '<span data-notify="message">{2}</span>' +
+          '<div class="progress" data-notify="progressbar">' +
+            '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+          '</div>' +
+          '<a href="{3}" target="{4}" data-notify="url"></a>' +
+        '</div>'
+    });
+}
+showNotification2(from, align){
+  
+    
+  $.notify({
+      icon: "danger",
+      message: "Check the Stocks on your medicines."
+
+  },{
+      type: 'danger',
+      timer: 4000,
+      placement: {
+          from: from,
+          align: align
+      },
+      template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+        '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+        '<i class="material-icons" data-notify="icon">notifications</i> ' +
+        '<span data-notify="title">{1}</span> ' +
+        '<span data-notify="message">{2}</span>' +
+        '<div class="progress" data-notify="progressbar">' +
+          '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+        '</div>' +
+        '<a href="{3}" target="{4}" data-notify="url"></a>' +
+      '</div>'
+  });
+}
+
+}
+
+
+export class NgbdProgressbarBasic {
+}
+
